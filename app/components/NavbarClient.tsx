3c33f7a54bff60b6515db92e32a565afb14c857b
unknown <mithunp336@gmail.com>
2026-01-04 16:48:19 +0530

    fix: video compression quality balance

--- app/components/NavbarClient.tsx
+++ app/components/NavbarClient.tsx
@@ -6,10 +6,6 @@ const Navbar = dynamic(
   () => import("./Navbar"),
   { ssr: false }
 );
-console.log(
-  "CLERK KEY EXISTS:",
-  !!process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
-);
 
 
 export default function NavbarClient() {
